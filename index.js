806539dc42099e62132bd00527e66c20dafd0550
rafatec7 <62843642+rafatec7@users.noreply.github.com>
2025-01-08 19:53:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const makeCommits = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const date = moment()
-    .subtract(4, "y")
+    .subtract(5, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -74,4 +74,4 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(268);
+makeCommits(258);
